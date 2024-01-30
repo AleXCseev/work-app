4f1d5528de7edb1a0790736f80b03627b344a477
alekseev konstantin <alexcseev1990@gmail.com>
2024-01-30 23:22:58 +0200

    add breeze

--- routes/web.php
+++ routes/web.php
@@ -1,22 +1,31 @@
 <?php
 
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
 
 Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/workers', [App\Http\Controllers\WorkerController::class, 'index'])->name('worker.index');
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
 
-Route::get('/workers/create', [App\Http\Controllers\WorkerController::class, 'create'])->name('worker.create');
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
 
-Route::get('/workers/{worker}', [App\Http\Controllers\WorkerController::class, 'show'])->name('worker.show');
-
-Route::post('/workers', [App\Http\Controllers\WorkerController::class, 'store'])->name('worker.store');
-
-Route::get('/workers/{worker}/edit', [App\Http\Controllers\WorkerController::class, 'edit'])->name('worker.edit');
-
-Route::patch('/workers/{worker}', [App\Http\Controllers\WorkerController::class, 'update'])->name('worker.update');
-
-Route::delete('/workers/{worker}', [App\Http\Controllers\WorkerController::class, 'delete'])->name('worker.delete');
-
+require __DIR__.'/auth.php';
